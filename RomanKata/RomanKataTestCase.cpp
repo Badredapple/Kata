e7caf01f37f97de9500c1305c883b4ab214d0a6d
badredapple <1197236910@qq.com>
2022-06-19 19:56:35 +0800

    refactor I X convert

--- RomanKata/RomanKataTestCase.cpp
+++ RomanKata/RomanKataTestCase.cpp
@@ -33,3 +33,34 @@ TEST(RomanKataTestCase, 20_convertto_XX){
 TEST(RomanKataTestCase, 30_convertto_XXX){
 	ASSERT_EQ("XXX", convert(30));
 }
+
+class RomanNumeralAssert{
+	public:
+		RomanNumeralAssert() = delete;
+		explicit RomanNumeralAssert(const unsigned int arabicNumber) :
+			arabicNumberToConvert(arabicNumber){ }
+		void isConvertedtoNumber(const std::string& expectedRomanNumeral) const {
+			ASSERT_EQ(expectedRomanNumeral, convert(arabicNumberToConvert));
+		}
+	private:
+		const unsigned int arabicNumberToConvert;
+};
+
+RomanNumeralAssert assertThat(const unsigned int arabicNumber){
+	RomanNumeralAssert assert { arabicNumber };
+	return assert;
+}
+
+TEST(RomanKataTestCase, 33_convertto_XXXIII){
+	assertThat(32).isConvertedtoNumber("XXXII");
+}
+
+TEST(RomanKataTestCase, conversionOfArabicNumbers){
+	assertThat(1).isConvertedtoNumber("I");
+	assertThat(2).isConvertedtoNumber("II");
+	assertThat(3).isConvertedtoNumber("III");
+	assertThat(10).isConvertedtoNumber("X");
+	assertThat(20).isConvertedtoNumber("XX");
+	assertThat(30).isConvertedtoNumber("XXX");
+	assertThat(32).isConvertedtoNumber("XXXII");
+}
