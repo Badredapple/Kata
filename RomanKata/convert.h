0a7188169e84c08fae50d621c454688c68361b7e
badredapple <1197236910@qq.com>
2022-06-19 20:13:59 +0800

    add std::array

--- RomanKata/convert.h
+++ RomanKata/convert.h
@@ -1,22 +1,35 @@
 #include <string>
 
+struct ArabicToRomanMapping{
+	unsigned int arabicNumber;
+	std::string romanNumeral;
+};
+
+const std::size_t numberOfMappings = 3;
+using ArabicToRomanMappings = std::array<ArabicToRomanMapping, numberOfMappings>;
+const ArabicToRomanMappings arabicToRomanMappings = {{
+	{100, "C"},
+	{ 10, "X"},
+	{  1, "I"}
+}};
+
 std::string convert(unsigned int number)
 {
 	std::string romanNumber;
-	while(number >= 100)
+	while(number >= arabicToRomanMappings[0].arabicNumber)
 	{
-		romanNumber += "C";
-		number-=100;
+		romanNumber += arabicToRomanMappings[0].romanNumeral;
+		number      -= arabicToRomanMappings[0].arabicNumber;
 	}	
-	while(number >= 10)
+	while(number >= arabicToRomanMappings[1].arabicNumber)
 	{
-		romanNumber += "X";
-		number-=10;
+		romanNumber += arabicToRomanMappings[1].romanNumeral;
+		number      -= arabicToRomanMappings[1].arabicNumber;
 	}	
-	while(number >= 1)
+	while(number >= arabicToRomanMappings[2].arabicNumber)
 	{
-		romanNumber += "I";
-		number--;
+		romanNumber += arabicToRomanMappings[2].romanNumeral;
+		number      -= arabicToRomanMappings[2].arabicNumber;
 	}
 	return romanNumber;
 }
