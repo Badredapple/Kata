38995d87cb844631c0543929010db588f3eb6680
badredapple <1197236910@qq.com>
2022-06-20 23:44:22 +0800

    refactor score by Law 6

--- BowlingKata/bowling.h
+++ BowlingKata/bowling.h
@@ -1,11 +1,24 @@
 
+#include <vector>
+#include <iostream>
+
 class Game{
     public:
-        int roll(int pins) 
+        void roll(int pins) 
         {  
-            score += pins;
+            rolls.push_back(pins);
         }
-        int total_score() { return score; }
+
+        int total_score() 
+        { 
+            int score = 0;
+            for(int i = 0; i < rolls.size(); i++)
+            {
+                score += rolls[i];
+            }
+            return score; 
+        }
     private:
-        int score = 0;
+        std::vector<int> rolls;
+        int currentRoll = 0;
 };
