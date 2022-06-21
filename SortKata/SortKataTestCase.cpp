2bd4414257952b2467b62ba2daad41584e61e034
badredapple <1197236910@qq.com>
2022-06-21 23:02:39 +0800

    init sort_2

--- SortKata/SortKataTestCase.cpp
+++ SortKata/SortKataTestCase.cpp
@@ -17,13 +17,13 @@ TEST(SortKataTestCase, Sort_1){
     EXPECT_EQ(asList, sort(asList));
 }
 
-TEST(SortKataTestCase, Sort_2){
+TEST(SortKataTestCase, Sort_1_2){
     vector<int> asList  = {2, 1};
     vector<int> tarList = {1, 2};
     EXPECT_EQ(tarList, sort(asList));
 }
 
-TEST(SortKataTestCase, Sort_3){
+TEST(SortKataTestCase, Sort_1_3){
     vector<int> asList1  = {1, 2, 3};
     vector<int> asList2  = {2, 1, 3};
     vector<int> asList3  = {2, 3, 1};
@@ -39,4 +39,25 @@ TEST(SortKataTestCase, Sort_3){
     vector<int> asList5  = {9,7,8,6,5,4, 3, 2, 1};
     vector<int> tarList1 = {1,2,3,4,5,6,7,8,9};
     EXPECT_EQ(tarList1, sort(asList5));
+}
+
+TEST(SortKataTestCase, Sort_2_2){
+    vector<int> asList  = {2, 1};
+    vector<int> tarList = {1, 2};
+    GTEST_SUCCEED();
+    EXPECT_EQ(tarList, sort_2(asList));
+}
+
+TEST(SortKataTestCase, Sort_2_3){
+    vector<int> asList1  = {1, 2, 3};
+    // vector<int> asList2  = {2, 1, 3};
+    // vector<int> asList3  = {2, 3, 1};
+    // vector<int> asList4  = {3, 2, 1};
+
+    vector<int> tarList  = {1, 2, 3};
+
+    EXPECT_EQ(tarList, sort_2(asList1));
+    // EXPECT_EQ(tarList, sort(asList2));
+    // EXPECT_EQ(tarList, sort(asList3));
+    // EXPECT_EQ(tarList, sort(asList4));
 }
--- SortKata/sort1.h
+++ SortKata/sort1.h
@@ -22,3 +22,50 @@ vector<int> sort(vector<int> &vec)
     }
     return vec;
 }
+
+vector<int> sort_2(vector<int> &vec)
+{
+    vector<int> new_vec;
+    if(vec.size() <= 1)
+    {
+        return vec;
+    }else if(vec.size() == 2){
+        int first = vec[0];
+        int second = vec[1];
+        if(first > second)
+            return new_vec = {second, first};
+        else
+            return new_vec = {first, second};
+    }else{
+        int first = vec[0];
+        int middle = vec[0];
+        int last  = vec[0];
+        vector<int> lessers;
+        vector<int> greaters;
+
+        if(first < middle)
+            lessers.push_back(first);
+        if(last < middle)
+            lessers.push_back(last);
+        if(first > middle)
+            greaters.push_back(first);
+        if(last > middle)
+            greaters.push_back(last);
+        
+        vector<int> result;
+        addAll(result, sort_2(lessers));
+        result.push_back(middle);
+        addAll(result, sort_2(greaters));
+        return result;
+    }
+
+    return vec;
+}
+
+void addAll(vector<int> &a, vector<int> &b)
+{
+    for(int i = 0; i < b.size(); i++)
+    {
+        a.push_back(b[i]);
+    }
+}
