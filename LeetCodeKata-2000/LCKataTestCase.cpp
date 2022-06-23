4561dce72aa0a499ef78471a1729104c0ac423c9
badredapple <1197236910@qq.com>
2022-06-23 23:47:03 +0800

    finish LC-2000 Kata

--- LeetCodeKata-2000/LCKataTestCase.cpp
+++ LeetCodeKata-2000/LCKataTestCase.cpp
@@ -28,5 +28,23 @@ TEST(LCKataTestCase, LC_2000_2)
     char    ch1  =  'b';
     string ans1  = "baa";
 
-    EXPECT_EQ(true,checkout1(word1, ch1));
+    EXPECT_EQ(false,checkout1(word1, ch1));
+}
+
+TEST(LCKataTestCase, LC_2000_3)
+{
+    string word2 = "abcdef";
+    char    ch2  =  'b';
+    string ans2  = "fedcba";
+
+    EXPECT_EQ(ans2,checkout2(word2, ch2));
+}
+
+TEST(LCKataTestCase, LC_2000)
+{
+    string word2 = "abcdef";
+    char    ch2  =  'b';
+    string ans2  = "bacdef";
+
+    EXPECT_EQ(ans2,checkout3(word2, ch2));
 }
--- LeetCodeKata-2000/Solution.h
+++ LeetCodeKata-2000/Solution.h
@@ -16,3 +16,21 @@ bool checkout1(string &str1, char &ch)
     else 
         return false;
 }
+
+string checkout2(string &str1, char &ch)
+{
+    // str1 to char array:
+    reverse(str1.begin(), str1.end());
+    return str1;
+}
+
+string checkout3(string &str1, char &ch)
+{
+    // str1 to char array:
+    int index = str1.find(ch);
+    if(index != string::npos){
+        reverse(str1.begin(), str1.begin() + index + 1);
+    }
+
+    return str1;
+}
