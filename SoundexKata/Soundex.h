7c07dc2dafaa7ef91c07e117a4984c810afa5591
badredapple <1197236910@qq.com>
2022-06-24 22:41:13 +0800

    init encode and zeropad

--- SoundexKata/Soundex.h
+++ SoundexKata/Soundex.h
@@ -3,6 +3,10 @@
 class Soundex{
     public:
         std::string encode(const std::string &word) const{
-            return "A";
+            return word + "000";
+        }
+
+        std::string zeroPad(const std::string &word) const{
+            return word + "000";
         }
 };
--- SoundexKata/SoundexKataTestCase.cpp
+++ SoundexKata/SoundexKataTestCase.cpp
@@ -1,15 +1,19 @@
 #include <gmock/gmock.h>
 #include "Soundex.h"
-
-int main(int agrc, char ** argv)
-{
-	testing::InitGoogleTest(&agrc, argv);
-	return RUN_ALL_TESTS();
-}
+using testing::Eq;
 
 TEST(SoundexEncoding, RetainsSoleLetterOfOneLetterWord)
 {
     Soundex soundex;
     auto encoded = soundex.encode("A");
-    ASSERT_THAT(encoded, testing::Eq("A"));
+    ASSERT_THAT(encoded, Eq("A000"));
+}
+
+TEST(SoundexEncoding, PadsWithZerosToEnSureTreeDigits)
+{
+    Soundex soundex;
+    auto encoded = soundex.zeroPad("I");
+    ASSERT_THAT(encoded, Eq("I000"));
+    auto encoded = soundex.zeroPad("A");
+    ASSERT_THAT(encoded, Eq("A000"));
 }
