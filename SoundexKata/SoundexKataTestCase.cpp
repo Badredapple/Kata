52f1b49e6906be7b461b57ee853ade71ee07ab08
badredapple <1197236910@qq.com>
2022-06-24 23:23:23 +0800

    add IgnoresAlphabetics

--- SoundexKata/SoundexKataTestCase.cpp
+++ SoundexKata/SoundexKataTestCase.cpp
@@ -21,4 +21,12 @@ TEST_F(SoundexEncoding, PadsWithZerosToEnSureTreeDigits)
 TEST_F(SoundexEncoding, ReplacesConsonantsWithAppropriareDigits)
 {
     ASSERT_THAT(soundex.encode("Ab"), Eq("A100"));
+    ASSERT_THAT(soundex.encode("Ac"), Eq("A200"));
+    ASSERT_THAT(soundex.encode("Ad"), Eq("A300"));
+    ASSERT_THAT(soundex.encode("Ax"), Eq("A200"));
+}
+
+TEST_F(SoundexEncoding, IgnoresNonAlphabetics)
+{
+     ASSERT_THAT(soundex.encode("A#"), Eq("A000"));
 }
